fbae06230ee49956ff357f3bd8df13c679cee93c
Juan Fumero <ingjef@gmail.com>
2024-02-25 23:58:07 +0100

    delete old file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './curriculum/home/home.component';
 
 export const routes: Routes = [    
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    {
-      path: 'home',
-      component: HomeComponent
-    }
+    // { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // {
+    //   path: 'home',
+    //   component: HomeComponent
+    // }
   ];
